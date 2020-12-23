e0221a2574ff90eff15160204d38dad78d6058d2
Daniel Ferguson <thedanielfergusonkid@gmail.com>
2020-12-23 22:54:17 +1100

    OpenGraph and Twitter meta tags for articles

--- pages/articles/_slug.vue
+++ pages/articles/_slug.vue
@@ -18,6 +18,14 @@
   </div>
 </template>
 
+<!-- Twitter Meta Tags -->
+<meta name="twitter:card" content="summary_large_image">
+<meta property="twitter:domain" content="danferg.com">
+<meta property="twitter:url" content="https://danferg.com/articles/a-better-brighter-cleaner-future">
+<meta name="twitter:title" content="A better, brighter, cleaner future. | Dan Ferguson">
+<meta name="twitter:description" content="DESCRIPTION">
+<meta name="twitter:image" content="IMAGE HERE">
+
 <script>
 export default {
   head() {
@@ -30,7 +38,62 @@ export default {
           hid: "description",
           name: "description",
           content: article.description
-        }
+        },
+        {
+          hid: "og:url",
+          property: "og:url",
+          content: "https://danferg.com" + article.path,
+        },
+        {
+          hid: "og:type",
+          property: "og:type",
+          content: "article",
+        },
+        {
+          hid: "og:title",
+          property: "og:title",
+          content: article.title,
+        },
+        {
+          hid: "og:description",
+          property: "og:description",
+          content: article.description,
+        },
+        {
+          hid: "og:image",
+          property: "og:image",
+          content: "https://danferg.com/" + article.featured_image,
+        },
+        {
+          hid: "twitter:card",
+          property: "twitter:card",
+          content: "https://danferg.com/" + article.featured_image,
+        },
+        {
+          hid: "twitter:domain",
+          property: "twitter:domain",
+          content: "https://danferg.com",
+        },
+        {
+          hid: "twitter:url",
+          property: "twitter:url",
+          content: "https://danferg.com" + article.path,
+        },
+        {
+          hid: "twitter:title",
+          property: "twitter:title",
+          content: article.title,
+        },
+        {
+          hid: "twitter:description",
+          property: "twitter:description",
+          content: article.description,
+        },
+        {
+          hid: "twitter:image",
+          property: "twitter:image",
+          content: "https://danferg.com/" + article.featured_image,
+        },
       ]
     };
   },
